d9d81f950be7674eade1222fd16fda338a5ec003
ChistoCode <118849296+ChistoCode@users.noreply.github.com>
2023-12-13 19:00:07 +0300

    finished making PrefabCreator
    ofc

--- Assets/Scripts/PrefabCreator/PrefabCreator.cs
+++ Assets/Scripts/PrefabCreator/PrefabCreator.cs
@@ -2,22 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-using System;
+using JetBrains.Annotations;
 
 public static class PrefabCreator
 {
     private static Dictionary<string, MonoBehaviour> _prefabs = new Dictionary<string, MonoBehaviour>();
 
-    static void Create<T>() where T : MonoBehaviour
+    [CanBeNull]
+    public static T Create<T>(Transform parent = null) where T : MonoBehaviour
     {
+        
         var classType = typeof(T);
         var attributes = classType.GetCustomAttributes(false);
         var prefabAttribute = attributes.FirstOrDefault(x => x is DefaultPrefabAttribute) as DefaultPrefabAttribute;
         if(prefabAttribute == null)
         {
-            throw new Exception($"[{nameof(PrefabCreator)}] cant find attribute: {nameof(DefaultPrefabAttribute)} on {nameof(T)}");
+            Debug.LogError($"[{nameof(PrefabCreator)}] cant find attribute: {nameof(DefaultPrefabAttribute)} on {classType.Name}");
+            return null;
         }
         var path = prefabAttribute.PrefabPath;
+
+        if (_prefabs.TryGetValue(path, out var prefab))
+            return Object.Instantiate(prefab, parent) as T;
+
+        prefab = Resources.Load<T>(path);
+        if(prefab == null)
+        {
+            Debug.LogError($"[{nameof(PrefabCreator)}] cant find prefab: {classType.Name} on {path}");
+            return null;
+        }
+        _prefabs.Add(path, prefab);
+        return Object.Instantiate(prefab, parent) as T;
     }
   
 }
